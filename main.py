379fe039fba99879734df1c710460fba7e2dd623
Frknk <frknk1@protonmail.com>
2024-01-08 10:07:02 -0500

    style: formatting project

--- main.py
+++ main.py
@@ -3,13 +3,15 @@ import jwt
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return render_template('index.html')
 
-@app.route('/decode_token', methods=['POST'])
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/decode_token", methods=["POST"])
 def decode_token():
-    token = request.json['token']
+    token = request.json["token"]
     try:
         decoded = jwt.decode(token, options={"verify_signature": False})
         return jsonify(decoded)
@@ -17,5 +19,6 @@ def decode_token():
         print(f"Error decoding token: {e}")
         return jsonify(error=str(e)), 400
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run()
--- static/index.js
+++ static/index.js
@@ -1,34 +1,37 @@
 function handleCredentialResponse(response) {
-    console.log("handleCredentialResponse called")
+  console.log("handleCredentialResponse called");
 
-    // Send the ID token to your server
-    fetch('/decode_token', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-            token: response.credential,
-        }),
-    })
-    .then(response => response.json())
-    .then(data => {
-        console.log("ID: " + data.sub);
-        console.log('Full Name: ' + data.name);
-        console.log('Given Name: ' + data.given_name);
-        console.log('Family Name: ' + data.family_name);
-        console.log("Image URL: " + data.picture);
-        console.log("Email: " + data.email);
-        var element = document.getElementById("user-info");
-        element.style.display = 'flex';
-        document.getElementById('id').textContent = "ID: " + data.sub;
-        document.getElementById('name').textContent = 'Full Name: ' + data.name;
-        document.getElementById('given_name').textContent = 'Given Name: ' + data.given_name;
-        document.getElementById('family_name').textContent = 'Family Name: ' + data.family_name;
-        document.getElementById('picture').textContent = "Image URL: " + data.picture;
-        document.getElementById('email').textContent = "Email: " + data.email;
+  // Send the ID token to your server
+  fetch("/decode_token", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      token: response.credential,
+    }),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log("ID: " + data.sub);
+      console.log("Full Name: " + data.name);
+      console.log("Given Name: " + data.given_name);
+      console.log("Family Name: " + data.family_name);
+      console.log("Image URL: " + data.picture);
+      console.log("Email: " + data.email);
+      var element = document.getElementById("user-info");
+      element.style.display = "flex";
+      document.getElementById("id").textContent = "ID: " + data.sub;
+      document.getElementById("name").textContent = "Full Name: " + data.name;
+      document.getElementById("given_name").textContent =
+        "Given Name: " + data.given_name;
+      document.getElementById("family_name").textContent =
+        "Family Name: " + data.family_name;
+      document.getElementById("picture").textContent =
+        "Image URL: " + data.picture;
+      document.getElementById("email").textContent = "Email: " + data.email;
     })
     .catch((error) => {
-        console.error('Error:', error);
+      console.error("Error:", error);
     });
 }
